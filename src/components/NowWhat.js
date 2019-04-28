c8f660354006ccc3cc812d338932183ad7a66556
SrinivasReddy <85804.pinninti@students.itu.edu>
2019-04-28 19:41:28 -0400

    1st commit

---
+++ src/components/NowWhat.js
@@ -0,0 +1,149 @@
+import React, { Component } from "react";
+import Card from "@material-ui/core/Card";
+import CardHeaderRaw from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import { withStyles } from "@material-ui/core/styles";
+import AvatarRaw from "@material-ui/core/Avatar";
+import {
+  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+} from 'recharts';
+
+import { connect } from "react-redux";
+import * as actions from "../store/actions";
+import LinearProgress from "@material-ui/core/LinearProgress";
+
+const cardStyles = theme => ({
+  root: {
+    background: theme.palette.primary.main
+  },
+  title: {
+    color: "white"
+  }
+});
+const CardHeader = withStyles(cardStyles)(CardHeaderRaw);
+
+const avatarStyles = theme => ({
+  root: {
+    background: theme.palette.primary.main
+  },
+  title: {
+    color: "white"
+  }
+});
+const Avatar = withStyles(avatarStyles)(AvatarRaw);
+
+const styles = {
+  card: {
+    margin: "5% 25%"
+  }
+};
+
+let firstTime = true;
+
+class NowWhat extends Component {
+  componentDidMount() {
+    this.props.onLoad();
+  }
+
+  render() {
+    const {
+          loading,
+          name,
+          latitude,
+          longitude,
+          temperature,
+          classes
+        } = this.props;
+
+
+    let data = [
+      {
+        name: 'Current State', temperature: temperature, latitude: latitude, longitude: longitude
+      }
+    ];
+
+    if (loading && firstTime) {
+        firstTime = false;
+        return <LinearProgress />;
+    }
+
+      return (
+        <Card className={classes.card}>
+          <CardContent>
+            <List>
+              <ListItem>
+                <ListItemText primary="Temperature:" />
+                <ListItemText primary={ temperature } />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Latitude:" />
+                <ListItemText primary={ latitude} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Longitude:" />
+                <ListItemText primary={longitude} />
+              </ListItem>
+            </List>
+          </CardContent>
+
+          <LineChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+              top: 5, right: 30, left: 20, bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="temperature" stroke="#8884d8" activeDot={{ r: 8 }} />
+            <Line type="monotone" dataKey="latitude" stroke="#82ca9d" />
+            <Line type="monotone" dataKey="longitude" stroke="#ab0000" />
+          </LineChart>
+
+        </Card>
+      );
+  }
+}
+
+const mapState = (state, ownProps) => {
+  const {
+    loading,
+    name,
+    latitude,
+    longitude,
+    temperature
+  } = state.weather;
+  return {
+    loading,
+    name,
+    latitude,
+    longitude,
+    temperature
+  };
+};
+
+const mapDispatch = dispatch => ({
+  onLoad: () => {
+    dispatch({
+      type: actions.FETCH_DRONE
+    })
+
+    setInterval(function(){
+      dispatch({
+        type: actions.FETCH_DRONE
+      })
+    }, 5000 );
+  }
+});
+
+export default connect(
+  mapState,
+  mapDispatch
+)(withStyles(styles)(NowWhat));
---
+++ src/store/reducers/Weather.js
@@ -0,0 +1,78 @@
+import * as actions from "../actions";
+
+const initialState = {
+  loading: false,
+  weatherId: null,
+  name: "",
+  temperature: "",
+  weather_state_name: "",
+  latitude: null,
+  longitude: null,
+  data: {}
+};
+
+const toF = c => (c * 9) / 5 + 32;
+
+const startLoading = (state, action) => {
+  return { ...state, loading: true };
+};
+
+const weatherIDReceived = (state, action) => {
+  return { ...state, weatherId: action.id };
+};
+
+const weatherDataRecevied = (state, action) => {
+  const { data } = action;
+  if (!data["consolidated_weather"]) return state;
+  const weather = data.consolidated_weather[0];
+  const { weather_state_name, the_temp } = weather;
+  const { latt_long, title: name } = data;
+  const [latitude, longitude] = latt_long.split(",");
+
+  return {
+    ...state,
+    loading: false,
+    latitude,
+    longitude,
+    temperatureinCelsius: the_temp,
+    temperatureinFahrenheit: toF(the_temp),
+    weather_state_name,
+    name,
+    data: action.data
+  };
+};
+
+const droneDataReceived = (state, action) => {
+  const data = action;
+
+  if (!data["fullArr"]) return state;
+  const latitude = data.fullArr[ data.fullArr.length-1 ].latitude;
+  const longitude = data.fullArr[ data.fullArr.length-1 ].longitude;
+  const temperature = data.fullArr[ data.fullArr.length-1 ].metric;
+
+  let obj = {
+    ...state,
+    loading: false,
+    latitude,
+    longitude,
+    temperature
+  };
+
+  console.log(obj);
+
+  return obj;
+};
+
+const handlers = {
+  [actions.FETCH_WEATHER]: startLoading,
+  [actions.WEATHER_ID_RECEIVED]: weatherIDReceived,
+  [actions.WEATHER_DATA_RECEIVED]: weatherDataRecevied,
+  [actions.FETCH_DRONE]: startLoading,
+  [actions.DRONE_DATA_RECEIVED]: droneDataReceived
+};
+
+export default (state = initialState, action) => {
+  const handler = handlers[action.type];
+  if (typeof handler === "undefined") return state;
+  return handler(state, action);
+};
